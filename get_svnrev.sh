2aedb157b8f03b2c18cf088b00c56fca1bbb82e9
Andreas.Fink85 <Andreas.Fink85@121b4492-b84c-0410-8b4c-0d4edfb3f3cc>
2010-06-14 13:01:13 +0000

    *fix* use [ ] instead of [[ ]] because some systems don't know about [[ ]]???
    git-svn-id: http://tint2.googlecode.com/svn/trunk@501 121b4492-b84c-0410-8b4c-0d4edfb3f3cc

--- get_svnrev.sh
+++ get_svnrev.sh
@@ -2,27 +2,26 @@
 
 FALLBACK=\"0.10-svn\"
 
-if [[ $# -eq 0 ]]; then
+if [ $# -eq 0 ]; then
   DIR=.
 else
   DIR=$1
 fi
 
-if [[ -f version.h ]]; then
+if [ -f version.h ]; then
   REV_OLD=$(cat version.h  | cut -d" " -f3)
 else
-  REV_OLD=""
+  REV_OLD=\"\"
 fi
 
-if [[ -x "$(which svnversion 2>/dev/null)" && -d "${DIR}/.svn" ]] ; then
+if [ -x "$(which svnversion 2>/dev/null)" -a -d "${DIR}/.svn" ] ; then
   REV=\"$(svnversion -n ${DIR})\"
 else
   REV=${FALLBACK}
 fi
 
-if [[ ${REV_OLD} != ${REV} ]]; then
+if [ ${REV_OLD} != ${REV} ]; then
   echo "Building new version.h"
+  echo "Rev_old: ${REV_OLD} Rev: ${REV}"
   echo "#define VERSION_STRING ${REV}" > version.h
 fi
-
-
