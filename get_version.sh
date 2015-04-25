0d7dfc082d2f45ab1ec772e198ae29af00731cbb
o9000 <o9000>
2015-04-25 22:57:06 +0200

    Update git version script

--- get_version.sh
+++ get_version.sh
@@ -32,3 +32,4 @@ fi
 VERSION=$(git describe --exact-match 2>/dev/null || echo "0.11-git$(git show -s --pretty=format:%cI.%h | tr -d ':' | tr -d '-' | tr '.' '-' | sed 's/T[0-9\+]*//g')")$DIRTY
 
 echo '#define VERSION_STRING "'$VERSION'"' > version.h
+echo $VERSION
