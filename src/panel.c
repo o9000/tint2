b9508450b7f573e85c3ced43ab63e28c2fb242dc
o9000 <o9000>
2015-05-03 09:27:52 +0200

    panel: Use WINDOW_TYPE_SPLASH instead of DOCK if panel_layer=normal and panel_dock=0 otherwise it is impossible to get normal stacking

--- src/panel.c
+++ src/panel.c
@@ -524,7 +524,9 @@ void set_panel_properties(Panel *p)
 	}
 
 	// Dock
-	long val = panel_dock ? server.atom._NET_WM_WINDOW_TYPE_DOCK : server.atom._NET_WM_WINDOW_TYPE_SPLASH;
+	long val = (!panel_dock && panel_layer == NORMAL_LAYER)
+			   ? server.atom._NET_WM_WINDOW_TYPE_SPLASH
+			   : server.atom._NET_WM_WINDOW_TYPE_DOCK;
 	XChangeProperty (server.dsp, p->main_win, server.atom._NET_WM_WINDOW_TYPE, XA_ATOM, 32, PropModeReplace, (unsigned char *) &val, 1);
 
 	val = ALLDESKTOP;
