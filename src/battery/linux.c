85b3b8a749713d37d24898e436766df4d5f56d35
o9000 <mrovi9000@gmail.com>
2015-09-12 12:22:32 +0200

    Battery: fix comment in estimate_power_usage

--- src/battery/linux.c
+++ src/battery/linux.c
@@ -285,7 +285,7 @@ static gint estimate_power_usage(struct psy_battery *bat, gint old_energy_now, g
 	gint64 diff_power = ABS(bat->energy_now - old_energy_now);
 	gint64 diff_time = bat->timestamp - old_timestamp;
 
-	/* µW = (µWh * 3600) / (µs * 1000000) */
+	/* µW = (µWh * 3600) / (µs / 1000000) */
 	gint power = diff_power * 3600 * 1000000 / MAX(1, diff_time);
 
 	return power;
