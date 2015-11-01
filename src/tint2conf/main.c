6d59e55d8d882a2659c59a5846558d90aeb39a5f
o9000 <mrovi9000@gmail.com>
2015-11-01 12:53:48 +0100

    tint2conf: Call setlocale with no locale to use system default instead of C

--- src/tint2conf/main.c
+++ src/tint2conf/main.c
@@ -124,6 +124,7 @@ static const char *global_ui =
 
 int main(int argc, char **argv)
 {
+	setlocale(LC_ALL, "");
 	bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
 	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
 	textdomain(GETTEXT_PACKAGE);
