995a257b2c1716d99fd2ca5979282388f49ab606
o9000 <mrovi9000@gmail.com>
2016-02-21 19:58:30 +0100

    Use standard macro to identify Linux (fixes build on powerpc)

--- src/battery/linux.c
+++ src/battery/linux.c
@@ -17,7 +17,7 @@
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **************************************************************************/
 
-#ifdef __linux
+#ifdef __linux__
 
 #include <stdlib.h>
 
