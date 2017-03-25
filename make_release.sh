46371fe81657e97cb2481c0436b5c72cd0839fbe
o9000 <mrovi9000@gmail.com>
2017-03-25 11:49:37 +0100

    Release 0.13.3

--- make_release.sh
+++ make_release.sh
@@ -2,24 +2,6 @@
 
 # Usage: ./make_release.sh
 # Creates a tar.gz archive of the current tree.
-#
-# To bump the version number for the current commit (make sure you are in HEAD!), run manually:
-#
-# git tag -a v0.12 -m 'Version 0.12'
-#
-# To generate a release for an older tagged commit, first list the tags with:
-#
-# git tag
-#
-# then checkout the tagged commit with:
-#
-# git checkout tags/v0.1
-#
-# Finally, to revert to HEAD:
-#
-# git checkout master
-#
-# See more at https://gitlab.com/o9000/tint2/wikis/Development
 
 VERSION=$(./get_version.sh --strict)
 if [ ! $? -eq 0 ]
@@ -28,21 +10,10 @@ then
     exit 1
 fi
 
-DIR=tint2-$VERSION
-ARCHIVE=$DIR.tar.gz
-echo "Making release $DIR"
-rm -rf $DIR $ARCHIVE
+ARCHIVE=tint2-$VERSION.tar.gz
 
-git checkout-index --prefix=$DIR/ -a
-
-# Delete unneeded files
-rm -f $DIR/make_release.sh
-
-echo "echo \"#define VERSION_STRING \\\"$VERSION\\\"\" > version.h" > $DIR/get_version.sh
-
-# Create tarball and remove the exported directory
-tar -czf $ARCHIVE $DIR
-rm -rf $DIR
+echo "Making release tint2-$VERSION"
+git archive --format=tar.gz --prefix=tint2-$VERSION/ v$VERSION >$ARCHIVE
 
 sha1sum -b $ARCHIVE
 sha256sum -b $ARCHIVE
