909d5ec139ed84eb4b8a3ada6f55e76986e4bf37
o9000 <mrovi9000@gmail.com>
2017-09-02 14:31:16 +0200

    Update screenshots

--- test/manual-theme-check.sh
+++ test/manual-theme-check.sh
@@ -12,5 +12,6 @@ do
   ( ( sleep 1 ; import -window tint2 ../../tint2.wiki/screenshots/${name}.png ; echo "Screenshot taken for ${name}!" ) &)
   ../build/tint2 -c $f
   sleep 1
-  echo "![${name}](screenshots/${name})\n" >> ../../tint2.wiki/screenshots.md
+  echo -e "### [${name}](https://gitlab.com/o9000/tint2/blob/master/themes/$(basename $f))\n" >> ../../tint2.wiki/screenshots.md
+  echo -e "![${name}](https://gitlab.com/o9000/tint2/wikis/screenshots/${name}.png)\n" >> ../../tint2.wiki/screenshots.md
 done
