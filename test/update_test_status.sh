fd0a1bef658311fdfc2fcef50901fb25c20537d2
o9000 <mrovi9000@gmail.com>
2017-09-28 18:51:07 +0200

    Regression testing

--- test/update_test_status.sh
+++ test/update_test_status.sh
@@ -18,10 +18,12 @@ git reset --hard
 git pull
 
 
-~/tint2/test/regression.py > tests.tmp.md
-cat tests.tmp.md > tests.md
-rm tests.tmp.md
+cd ~/tint2/test
+~/tint2/test/regression.py > ~/tint2.wiki/tests.tmp.md
+cat ~/tint2.wiki/tests.tmp.md > ~/tint2.wiki/tests.md
+rm ~/tint2.wiki/tests.tmp.md
 
+cd ~/tint2.wiki
 git add tests.md
 git commit -am 'Update test results'
 git push origin master
